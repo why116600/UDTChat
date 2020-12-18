c0f09c7e428aded4d43c25d1235373fce945e699
wanghongyuan <wanghy286@mail2.sysu.edu.cn>
2020-12-18 20:00:09 +0800

    穿越时绑定本地随机指定的端口

--- UDTChat/ChatClient.cs
+++ UDTChat/ChatClient.cs
@@ -15,10 +15,12 @@ namespace UDTChat
         Thread m_th = null;
         ChatForm m_form = null;
         bool m_bTranverse = false;
+        int m_bindPort = 1234;
 
         private ChatClient()
         {
-
+            Random r = new Random();
+            m_bindPort = r.Next(10000, 20000);
         }
 
         public static ChatClient Connect(string strAddress,int nPort,ChatForm form,bool bTranverse=false)
@@ -29,6 +31,10 @@ namespace UDTChat
             try
             {
                 cc.m_mainSkt = new Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream);
+                if(bTranverse)
+                {
+                    cc.m_mainSkt.Bind(System.Net.IPAddress.Any, cc.m_bindPort);
+                }
                 cc.m_mainSkt.Connect(strAddress, nPort);
                 cc.m_th = new Thread(new ThreadStart(cc.ClientThread));
                 cc.m_th.Start();
@@ -90,7 +96,7 @@ namespace UDTChat
                     ipaddr = new System.Net.IPAddress(BitConverter.ToInt64(buffer, 4));
                     skt = new Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream);
                     skt.SetSocketOption(SocketOptionName.Rendezvous, true);
-                    skt.Bind(m_mainSkt.LocalEndPoint);
+                    skt.Bind(System.Net.IPAddress.Any, m_bindPort);
                     skt.Connect(ipaddr, port);
                     m_tranSkt = m_mainSkt;
                     m_mainSkt = skt;
